a033689f02dd8fc2a4a0cf6a940689839b993f91
Evan Leach <220085087@aston.ac.uk>
2023-11-26 19:20:58 +0000

    Update 220085087_dml.sql
    Added image paths

--- 220085087_dml.sql
+++ 220085087_dml.sql
@@ -6,35 +6,35 @@ VALUES ('evan_leach', 'evanteam28@aston.com', 'hashed_password', 'customer');
 -- Populate the products table
 INSERT INTO products (title, description, price, genre, console_type, publisher, developer, stock_quantity, image_path) 
 VALUES 
-('The Legend of Zelda: Breath of the Wild', 'Adventure game for Nintendo Switch', 45.99, 'Adventure', 'Nintendo Switch', 'Nintendo', 'Nintendo', 100, '/path/to/zelda_image.jpg'),
-('Journey', 'Adventure game for PlayStation, PC', 14.99, 'Adventure', 'PlayStation, PC', 'Sony Interactive Entertainment', 'Santa Monica Studio', 100, '/path/to/journey_image.jpg'),
-('Uncharted 4: A Thief’s End', 'Adventure game for PlayStation', 45.99, 'Adventure', 'PlayStation', 'Sony Interactive Entertainment', 'Naughty Dog', 100, '/path/to/uncharted4_image.jpg'),
-('Firewatch', 'Adventure game for PlayStation, Xbox, PC', 14.99, 'Adventure', 'PlayStation, Xbox, PC', 'Panic', 'Campo Santo', 100, '/path/to/firewatch_image.jpg'),
-('Stardew Valley', 'Adventure game for Multiple Platforms', 11.99, 'Adventure', 'PlayStation, Xbox, PC, Nintendo Switch', 'ConcernedApe', 'ConcernedApe', 100, '/path/to/stardewvalley_image.jpg'),
+('The Legend of Zelda: Breath of the Wild', 'Adventure game for Nintendo Switch', 45.99, 'Adventure', 'Nintendo Switch', 'Nintendo', 'Nintendo', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/zelda.jpg?raw=true'),
+('Journey', 'Adventure game for PlayStation, PC', 14.99, 'Adventure', 'PlayStation, PC', 'Sony Interactive Entertainment', 'Santa Monica Studio', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Journey.jpg?raw=true'),
+('Uncharted 4: A Thief’s End', 'Adventure game for PlayStation', 45.99, 'Adventure', 'PlayStation', 'Sony Interactive Entertainment', 'Naughty Dog', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/uncharted4.jpg?raw=true'),
+('Firewatch', 'Adventure game for PlayStation, Xbox, PC', 14.99, 'Adventure', 'PlayStation, Xbox, PC', 'Panic', 'Campo Santo', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/firewatch.jpeg?raw=true'),
+('Stardew Valley', 'Adventure game for Multiple Platforms', 11.99, 'Adventure', 'PlayStation, Xbox, PC, Nintendo Switch', 'ConcernedApe', 'ConcernedApe', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/stardewvalley.png?raw=true'),
 -- Board Games
-('Catan Universe', 'Board game for Multiple Platforms', 0.00, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'United Soft Media', 'Exozet', 100, '/path/to/catanuniverse_image.jpg'),
-('Ticket to Ride', 'Board game for Multiple Platforms', 14.99, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'Days of Wonder', 'Days of Wonder', 100, '/path/to/tickettoride_image.jpg'),
-('Splendor', 'Board game for Multiple Platforms', 8.50, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'Space Cowboys', 'Asmodee Digital', 100, '/path/to/splendor_image.jpg'),
-('Carcassonne', 'Board game for Multiple Platforms', 3.99, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'Hans im Glück', 'Asmodee Digital', 100, '/path/to/carcassonne_image.jpg'),
-('Pandemic: The Board Game', 'Board game for Multiple Platforms', 0.99, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'Z-Man Games', 'Asmodee Digital', 100, '/path/to/pandemic_image.jpg'),
+('Catan Universe', 'Board game for Multiple Platforms', 0.00, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'United Soft Media', 'Exozet', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Catan_Universe.jpg?raw=true'),
+('Ticket to Ride', 'Board game for Multiple Platforms', 14.99, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'Days of Wonder', 'Days of Wonder', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/ticket_to_ride.jpg?raw=true'),
+('Splendor', 'Board game for Multiple Platforms', 8.50, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'Space Cowboys', 'Asmodee Digital', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/splendor.jpg?raw=true'),
+('Carcassonne', 'Board game for Multiple Platforms', 3.99, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'Hans im Glück', 'Asmodee Digital', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/carcassonne.jpg?raw=true'),
+('Pandemic: The Board Game', 'Board game for Multiple Platforms', 0.99, 'Board', 'PlayStation, Xbox, PC, Nintendo Switch', 'Z-Man Games', 'Asmodee Digital', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/pandemic.jpg?raw=true'),
 -- Puzzle Games
-('Tetris Effect', 'Puzzle game for Multiple Platforms', 19.99, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Enhance Games', 'Monstars Inc., Resonair', 100, '/path/to/tetriseffect_image.jpg'),
-('Portal 2', 'Puzzle game for Multiple Platforms', 8.50, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Valve Corporation', 'Valve Corporation', 100, '/path/to/portal2_image.jpg'),
-('The Witness', 'Puzzle game for Multiple Platforms', 9.99, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Thekla, Inc.', 'Thekla, Inc.', 100, '/path/to/thewitness_image.jpg'),
-('Monument Valley', 'Puzzle game for Multiple Platforms', 6.99, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Ustwo', 'Ustwo', 100, '/path/to/monumentvalley_image.jpg'),
-('Baba Is You', 'Puzzle game for Multiple Platforms', 14.99, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Hempuli', 'Hempuli', 100, '/path/to/babaisyou_image.jpg'),
+('Tetris Effect', 'Puzzle game for Multiple Platforms', 19.99, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Enhance Games', 'Monstars Inc., Resonair', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Tetris.jpg?raw=true'),
+('Portal 2', 'Puzzle game for Multiple Platforms', 8.50, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Valve Corporation', 'Valve Corporation', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Portal2cover.jpg?raw=true'),
+('The Witness', 'Puzzle game for Multiple Platforms', 9.99, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Thekla, Inc.', 'Thekla, Inc.', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/thewitness.jpg?raw=true'),
+('Monument Valley', 'Puzzle game for Multiple Platforms', 6.99, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Ustwo', 'Ustwo', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/monumentvalley.jpg?raw=true'),
+('Baba Is You', 'Puzzle game for Multiple Platforms', 14.99, 'Puzzle', 'PlayStation, Xbox, PC, Nintendo Switch', 'Hempuli', 'Hempuli', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/baba.jpg?raw=true'),
 -- Simulation Games
-('The Sims 4', 'Simulation game for Multiple Platforms', 0.00, 'Simulation', 'PlayStation, Xbox, PC, Nintendo Switch', 'Electronic Arts', 'Maxis', 100, '/path/to/sims4_image.jpg'),
-('Cities: Skylines', 'Simulation game for Multiple Platforms', 7.49, 'Simulation', 'PlayStation, Xbox, PC, Nintendo Switch', 'Paradox Interactive', 'Colossal Order', 100, '/path/to/citiesskylines_image.jpg'),
-('Microsoft Flight Simulator', 'Simulation game for PC, Xbox', 35.99, 'Simulation', 'PC, Xbox', 'Xbox Game Studios', 'Asobo Studio', 100, '/path/to/microsoftflightsimulator_image.jpg'),
-('Stellaris', 'Simulation game for Multiple Platforms', 10.49, 'Simulation', 'PlayStation, Xbox, PC, Nintendo Switch', 'Paradox Interactive', 'Paradox Development Studio', 100, '/path/to/stellaris_image.jpg'),
-('Planet Zoo', 'Simulation game for Multiple Platforms', 10.49, 'Simulation', 'PlayStation, Xbox, PC, Nintendo Switch', 'Frontier Developments', 'Frontier Developments', 100, '/path/to/planetzoo_image.jpg'),
+('The Sims 4', 'Simulation game for Multiple Platforms', 0.00, 'Simulation', 'PlayStation, Xbox, PC, Nintendo Switch', 'Electronic Arts', 'Maxis', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/thesims.jpg?raw=true'),
+('Cities: Skylines', 'Simulation game for Multiple Platforms', 7.49, 'Simulation', 'PlayStation, Xbox, PC, Nintendo Switch', 'Paradox Interactive', 'Colossal Order', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/citiesskylines.jpg?raw=true'),
+('Microsoft Flight Simulator', 'Simulation game for PC, Xbox', 35.99, 'Simulation', 'PC, Xbox', 'Xbox Game Studios', 'Asobo Studio', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Microsoft_Flight_Simulator.png?raw=true'),
+('Stellaris', 'Simulation game for Multiple Platforms', 10.49, 'Simulation', 'PlayStation, Xbox, PC, Nintendo Switch', 'Paradox Interactive', 'Paradox Development Studio', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Stellaris_cover.png?raw=true'),
+('Planet Zoo', 'Simulation game for Multiple Platforms', 10.49, 'Simulation', 'PlayStation, Xbox, PC, Nintendo Switch', 'Frontier Developments', 'Frontier Developments', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/zoo.jpeg?raw=true'),
 -- Sports Games
-('EA FC24', 'Sports game for Multiple Platforms', 29.99, 'Sports', 'PlayStation, Xbox, PC, Nintendo Switch', 'Electronic Arts', 'EA Vancouver', 100, '/path/to/fifa22_image.jpg'),
-('NBA 2K24', 'Sports game for Multiple Platforms', 22.49, 'Sports', 'PlayStation, Xbox, PC, Nintendo Switch', '2K Sports', 'Visual Concepts', 100, '/path/to/nba2k22_image.jpg'),
-('Rocket League', 'Sports game for Multiple Platforms', 0.00, 'Sports', 'PlayStation, Xbox, PC, Nintendo Switch', 'Psyonix', 'Psyonix', 100, '/path/to/rocketleague_image.jpg'),
-('Wii Sports', 'Sports game for Nintendo Wii', 30.99, 'Sports', 'Nintendo Wii', 'Nintendo', 'Nintendo EAD', 100, '/path/to/wiisports_image.jpg'),
-('Mario Kart 8 Deluxe', 'Sports game for Nintendo Switch', 49.99, 'Sports', 'Nintendo Switch', 'Nintendo', 'Nintendo EAD', 100, '/path/to/mariokart8deluxe_image.jpg');
+('EA FC24', 'Sports game for Multiple Platforms', 29.99, 'Sports', 'PlayStation, Xbox, PC, Nintendo Switch', 'Electronic Arts', 'EA Vancouver', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/eafc24.jpeg?raw=true'),
+('NBA 2K24', 'Sports game for Multiple Platforms', 22.49, 'Sports', 'PlayStation, Xbox, PC, Nintendo Switch', '2K Sports', 'Visual Concepts', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/nba2k24.png?raw=true'),
+('Rocket League', 'Sports game for Multiple Platforms', 0.00, 'Sports', 'PlayStation, Xbox, PC, Nintendo Switch', 'Psyonix', 'Psyonix', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/rocket.jpeg?raw=true'),
+('Wii Sports', 'Sports game for Nintendo Wii', 30.99, 'Sports', 'Nintendo Wii', 'Nintendo', 'Nintendo EAD', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/wii.jpeg?raw=true'),
+('Mario Kart 8 Deluxe', 'Sports game for Nintendo Switch', 49.99, 'Sports', 'Nintendo Switch', 'Nintendo', 'Nintendo EAD', 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/mariokart8.jpg?raw=true');
 
 -- Populate the orders table
 INSERT INTO orders (user_id, total_price, Status) 
