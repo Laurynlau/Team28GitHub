adac9bb192abb191ec70cd0d6f68bea70357d95e
Evan Leach <220085087@aston.ac.uk>
2023-11-27 01:02:44 +0000

    Update 220085087_ddl.sql
    - Created Indices for tables

--- 220085087_ddl.sql
+++ 220085087_ddl.sql
@@ -2,12 +2,13 @@
 -- Table for users
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(512) NOT NULL, -- Store hashed passwords
-    date_joined DATETIME DEFAULT CURRENT_TIMESTAMP,
-    last_updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    user_role ENUM('admin', 'customer') DEFAULT 'customer'
+    date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_role ENUM('admin', 'customer') DEFAULT 'customer' NOT NULL,
+    active BOOLEAN DEFAULT TRUE -- Indicates if the account is active
 );
 
 -- Table for products
@@ -15,29 +16,43 @@ CREATE TABLE products (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
-    price DECIMAL(10, 2) CHECK (price >= 0),
-    genre VARCHAR(100),
-    console_type VARCHAR(100),
+    price DECIMAL(10, 2) CHECK (price >= 0) NOT NULL,
+    genre_id INT,
+    console_type_id INT,
     release_date DATE,
     publisher_id INT,
     developer_id INT,
-    stock_quantity INT DEFAULT 0 CHECK (stock_quantity >= 0),
+    stock_quantity INT DEFAULT 0 CHECK (stock_quantity >= 0) NOT NULL,
     image_path VARCHAR(500),
-    last_updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id),
-    FOREIGN KEY (developer_id) REFERENCES developers(developer_id)
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id) ON DELETE CASCADE,
+    FOREIGN KEY (developer_id) REFERENCES developers(developer_id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genres(genre_id) ON DELETE SET NULL,
+    FOREIGN KEY (console_type_id) REFERENCES console_types(console_type_id) ON DELETE SET NULL
 );
 
 -- Table for publishers
 CREATE TABLE publishers (
     publisher_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL UNIQUE
 );
 
 -- Table for developers
 CREATE TABLE developers (
     developer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Table for genres
+CREATE TABLE genres (
+    genre_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+-- Table for console types
+CREATE TABLE console_types (
+    console_type_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
 );
 
 -- Table for orders
@@ -47,8 +62,10 @@ CREATE TABLE orders (
     order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     total_price DECIMAL(10, 2) NOT NULL,
     Status ENUM('completed', 'pending', 'cancelled') DEFAULT 'pending',
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_orders_user_id ON orders(user_id);
 
 -- Table for order details
 CREATE TABLE order_details (
@@ -57,9 +74,12 @@ CREATE TABLE order_details (
     product_id INT NOT NULL,
     quantity INT NOT NULL,
     price_per_unit DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_order_details_order_id ON order_details(order_id);
+CREATE INDEX idx_order_details_product_id ON order_details(product_id);
 
 -- Table for reviews
 CREATE TABLE reviews (
@@ -69,9 +89,12 @@ CREATE TABLE reviews (
     rating INT CHECK (rating BETWEEN 1 AND 5),
     comment TEXT,
     date_posted DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES products(product_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_reviews_product_id ON reviews(product_id);
+CREATE INDEX idx_reviews_user_id ON reviews(user_id);
 
 -- Table for wishlist
 CREATE TABLE wishlist (
@@ -79,9 +102,12 @@ CREATE TABLE wishlist (
     user_id INT NOT NULL,
     product_id INT NOT NULL,
     date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_wishlist_user_id ON wishlist(user_id);
+CREATE INDEX idx_wishlist_product_id ON wishlist(product_id);
 
 -- Table for previous orders
 CREATE TABLE previous_orders (
@@ -90,6 +116,9 @@ CREATE TABLE previous_orders (
     order_id INT NOT NULL,
     completion_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     total_price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_previous_orders_user_id ON previous_orders(user_id);
+CREATE INDEX idx_previous_orders_order_id ON previous_orders(order_id);
