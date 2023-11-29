6575588f36e913522b03f6552c6b08bc566b9391
Charlie-River <74555580+Charlie-River@users.noreply.github.com>
2023-11-29 10:17:12 +0000

    Add files via upload
    Used to connect the database to the basket page while testing

---
+++ connectdb.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "teamprojectv2";
+
+// Create connection
+try {
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+?>
