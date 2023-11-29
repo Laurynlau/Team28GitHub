3d30833ad82dcd535a24ecf57ddd2662642d58bf
Evan Leach <220085087@aston.ac.uk>
2023-11-29 08:02:02 +0000

    Update 220085087_dml.sql
    - Fixed publisher and developer ids in populated product table
    - Fixed temp table query

--- 220085087_dml.sql
+++ 220085087_dml.sql
@@ -82,32 +82,32 @@ VALUES
 INSERT INTO products (title, description, price, genre_id, console_type_id, publisher_id, developer_id, stock_quantity, image_path) 
 VALUES -- Adventure Games
 ('The Legend of Zelda: Breath of the Wild', 'Adventure game for Nintendo Switch', 45.99, 1, 1, 1, 1, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/zelda.jpg?raw=true'),
-('Journey', 'Adventure game for PlayStation, PC', 14.99, 1, 7, 2, 3, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Journey.jpg?raw=true'),
+('Journey', 'Adventure game for PlayStation, PC', 14.99, 1, 7, 2, 2, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Journey.jpg?raw=true'),
 ('Uncharted 4: A Thief’s End', 'Adventure game for PlayStation', 45.99, 1, 2, 2, 4, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/uncharted4.jpg?raw=true'),
 ('Firewatch', 'Adventure game for PlayStation, Xbox, PC', 14.99, 1, 9, 3, 5, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/firewatch.jpeg?raw=true'),
 ('Stardew Valley', 'Adventure game for Multiple Platforms', 11.99, 1, 6, 4, 6, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/stardewvalley.png?raw=true'),
 -- Board Games
-('Catan Universe', 'Board game for Multiple Platforms', 0.00, 2, 6, 5, 7, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Catan_Universe.jpg?raw=true'),
-('Ticket to Ride', 'Board game for Multiple Platforms', 14.99, 2, 6, 7, 8, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/ticket_to_ride.jpg?raw=true'),
-('Splendor', 'Board game for Multiple Platforms', 8.50, 2, 6, 8, 9, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/splendor.jpg?raw=true'),
-('Carcassonne', 'Board game for Multiple Platforms', 3.99, 2, 6, 9, 10, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/carcassonne.jpg?raw=true'),
-('Pandemic: The Board Game', 'Board game for Multiple Platforms', 0.99, 2, 6, 10, 10, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/pandemic.jpg?raw=true'),
+('Catan Universe', 'Board game for Multiple Platforms', 0.00, 2, 6, 5, 8, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Catan_Universe.jpg?raw=true'),
+('Ticket to Ride', 'Board game for Multiple Platforms', 14.99, 2, 6, 6, 8, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/ticket_to_ride.jpg?raw=true'),
+('Splendor', 'Board game for Multiple Platforms', 8.50, 2, 6, 7, 9, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/splendor.jpg?raw=true'),
+('Carcassonne', 'Board game for Multiple Platforms', 3.99, 2, 6, 8, 7, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/carcassonne.jpg?raw=true'),
+('Pandemic: The Board Game', 'Board game for Multiple Platforms', 0.99, 2, 6, 9, 9, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/pandemic.jpg?raw=true'),
 -- Puzzle Games
-('Tetris Effect', 'Puzzle game for Multiple Platforms', 19.99, 3, 6, 11, 11, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Tetris.jpg?raw=true'),
-('Portal 2', 'Puzzle game for Multiple Platforms', 8.50, 3, 6, 12, 12, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Portal2cover.jpg?raw=true'),
-('The Witness', 'Puzzle game for Multiple Platforms', 9.99, 3, 6, 13, 13, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/thewitness.jpg?raw=true'),
-('Monument Valley', 'Puzzle game for Multiple Platforms', 6.99, 3, 6, 14, 14, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/monumentvalley.jpg?raw=true'),
-('Baba Is You', 'Puzzle game for Multiple Platforms', 14.99, 3, 6, 15, 15, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/baba.jpg?raw=true'),
+('Tetris Effect', 'Puzzle game for Multiple Platforms', 19.99, 3, 6, 10, 10, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Tetris.jpg?raw=true'),
+('Portal 2', 'Puzzle game for Multiple Platforms', 8.50, 3, 6, 11, 11, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Portal2cover.jpg?raw=true'),
+('The Witness', 'Puzzle game for Multiple Platforms', 9.99, 3, 6, 12, 12, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/thewitness.jpg?raw=true'),
+('Monument Valley', 'Puzzle game for Multiple Platforms', 6.99, 3, 6, 13, 13, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/monumentvalley.jpg?raw=true'),
+('Baba Is You', 'Puzzle game for Multiple Platforms', 14.99, 3, 6, 14, 14, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/baba.jpg?raw=true'),
 -- Simulation Games
-('The Sims 4', 'Simulation game for Multiple Platforms', 0.00, 4, 6, 16, 16, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/thesims.jpg?raw=true'),
-('Cities: Skylines', 'Simulation game for Multiple Platforms', 7.49, 4, 6, 17, 17, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/citiesskylines.jpg?raw=true'),
-('Microsoft Flight Simulator', 'Simulation game for PC, Xbox', 35.99, 4, 3, 18, 18, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Microsoft_Flight_Simulator.png?raw=true'),
-('Stellaris', 'Simulation game for Multiple Platforms', 10.49, 4, 6, 17, 19, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Stellaris_cover.png?raw=true'),
-('Planet Zoo', 'Simulation game for Multiple Platforms', 10.49, 4, 6, 19, 19, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/zoo.jpeg?raw=true'),
+('The Sims 4', 'Simulation game for Multiple Platforms', 0.00, 4, 6, 15, 15, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/thesims.jpg?raw=true'),
+('Cities: Skylines', 'Simulation game for Multiple Platforms', 7.49, 4, 6, 16, 16, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/citiesskylines.jpg?raw=true'),
+('Microsoft Flight Simulator', 'Simulation game for PC, Xbox', 35.99, 4, 3, 17, 17, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Microsoft_Flight_Simulator.png?raw=true'),
+('Stellaris', 'Simulation game for Multiple Platforms', 10.49, 4, 6, 16, 18, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/Stellaris_cover.png?raw=true'),
+('Planet Zoo', 'Simulation game for Multiple Platforms', 10.49, 4, 6, 18, 19, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/zoo.jpeg?raw=true'),
 -- Sports Games
-('EA FC24', 'Sports game for Multiple Platforms', 29.99, 5, 6, 16, 20, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/eafc24.jpeg?raw=true'),
-('NBA 2K24', 'Sports game for Multiple Platforms', 22.49, 5, 6, 20, 21, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/nba2k24.png?raw=true'),
-('Rocket League', 'Sports game for Multiple Platforms', 0.00, 5, 6, 21, 21, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/rocket.jpeg?raw=true'),
+('EA FC24', 'Sports game for Multiple Platforms', 29.99, 5, 6, 15, 20, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/eafc24.jpeg?raw=true'),
+('NBA 2K24', 'Sports game for Multiple Platforms', 22.49, 5, 6, 19, 21, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/nba2k24.png?raw=true'),
+('Rocket League', 'Sports game for Multiple Platforms', 0.00, 5, 6, 20, 22, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/rocket.jpeg?raw=true'),
 ('Wii Sports', 'Sports game for Nintendo Wii', 30.99, 5, 5, 1, 23, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/wii.jpeg?raw=true'),
 ('Mario Kart 8 Deluxe', 'Sports game for Nintendo Switch', 49.99, 5, 1, 1, 23, 100, 'https://github.com/Laurynlau/Team28GitHub/blob/main/Game%20Images/mariokart8.jpg?raw=true');
 
@@ -129,13 +129,14 @@ VALUES (1, 1, '2023-01-15 08:00:00', 120.00);
 -- End of Transaction
 COMMIT;
 
--- Populate the reviews table
+-- Populate the reviews table 
+-- Product ids start from 51 to 75
 INSERT INTO reviews (product_id, user_id, rating, comment) 
-VALUES (1, 1, 5, 'Great game!');
+VALUES (51, 1, 5, 'Great game!');
 
 -- Populate the wishlist table
 INSERT INTO wishlist (user_id, product_id) 
-VALUES (1, 2);
+VALUES (1, 52);
 
 -- Update a user's email
 UPDATE users 
@@ -145,7 +146,7 @@ WHERE user_id = 1;
 -- Update a product's price
 UPDATE products 
 SET price = 49.99 
-WHERE product_id = 1; 
+WHERE product_id = 51; 
 
 -- Deleting a user (Soft delete approach) 
 -- To delete a user, uncomment and replace '1' with the user_id you want to delete
@@ -177,14 +178,14 @@ SET @upper_limit = 50.00;
 -- Create a temporary table to store the results
 CREATE TEMPORARY TABLE TempTable (
     product_id INT,
-    product VARCHAR(255),
+    title VARCHAR(255),
     price DECIMAL(10, 2)
 );
 
 -- Insert the products within the specified price range into the temporary table
-INSERT INTO TempTable (product_id, product, price)
-SELECT product_id, product, price
-FROM product
+INSERT INTO TempTable (product_id, title, price)
+SELECT product_id, title, price
+FROM products
 WHERE price >= @lower_limit AND price <= @upper_limit;
 
 -- Retrieve the results from the temporary table
