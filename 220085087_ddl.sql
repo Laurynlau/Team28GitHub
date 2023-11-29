2fb8f9b3bb3b62a8142af2edd34d69d1a2479150
Evan Leach <220085087@aston.ac.uk>
2023-11-29 07:34:51 +0000

    Update 220085087_ddl.sql
    - Database export from phpmyadmin

--- 220085087_ddl.sql
+++ 220085087_ddl.sql
@@ -1,124 +1,468 @@
--- DDL Evan Leach 220085087 Team 28
--- Table for users
-CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(512) NOT NULL, -- Store hashed passwords
-    date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    user_role ENUM('admin', 'customer') DEFAULT 'customer' NOT NULL,
-    active BOOLEAN DEFAULT TRUE -- Indicates if the account is active
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 29, 2023 at 07:10 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
--- Table for products
-CREATE TABLE products (
-    product_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) CHECK (price >= 0) NOT NULL,
-    genre_id INT,
-    console_type_id INT,
-    release_date DATE,
-    publisher_id INT,
-    developer_id INT,
-    stock_quantity INT DEFAULT 0 CHECK (stock_quantity >= 0) NOT NULL,
-    image_path VARCHAR(500),
-    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id) ON DELETE CASCADE,
-    FOREIGN KEY (developer_id) REFERENCES developers(developer_id) ON DELETE CASCADE,
-    FOREIGN KEY (genre_id) REFERENCES genres(genre_id) ON DELETE SET NULL,
-    FOREIGN KEY (console_type_id) REFERENCES console_types(console_type_id) ON DELETE SET NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Table for publishers
-CREATE TABLE publishers (
-    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE
-);
 
--- Table for developers
-CREATE TABLE developers (
-    developer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Table for genres
-CREATE TABLE genres (
-    genre_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE
-);
+--
+-- Database: `220085087_team28`
+--
 
--- Table for console types
-CREATE TABLE console_types (
-    console_type_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE
-);
+-- --------------------------------------------------------
 
--- Table for orders
-CREATE TABLE orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    total_price DECIMAL(10, 2) NOT NULL,
-    Status ENUM('completed', 'pending', 'cancelled') DEFAULT 'pending',
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
-);
+--
+-- Table structure for table `console_types`
+--
 
-CREATE INDEX idx_orders_user_id ON orders(user_id);
+CREATE TABLE `console_types` (
+  `console_type_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Table for order details
-CREATE TABLE order_details (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    price_per_unit DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
-);
+--
+-- Dumping data for table `console_types`
+--
 
-CREATE INDEX idx_order_details_order_id ON order_details(order_id);
-CREATE INDEX idx_order_details_product_id ON order_details(product_id);
+INSERT INTO `console_types` (`console_type_id`, `name`) VALUES
+(6, 'Multiple Platforms'),
+(1, 'Nintendo Switch'),
+(5, 'Nintendo Wii'),
+(4, 'PC'),
+(2, 'PlayStation'),
+(7, 'PlayStation and PC'),
+(10, 'PlayStation and Xbox'),
+(9, 'PlayStation, Xbox, and PC'),
+(11, 'PlayStation, Xbox, PC, and Nintendo Switch'),
+(3, 'Xbox'),
+(8, 'Xbox and PC');
 
--- Table for reviews
-CREATE TABLE reviews (
-    review_id INT AUTO_INCREMENT PRIMARY KEY,
-    product_id INT NOT NULL,
-    user_id INT NOT NULL,
-    rating INT CHECK (rating BETWEEN 1 AND 5),
-    comment TEXT,
-    date_posted DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
-);
+-- --------------------------------------------------------
 
-CREATE INDEX idx_reviews_product_id ON reviews(product_id);
-CREATE INDEX idx_reviews_user_id ON reviews(user_id);
+--
+-- Table structure for table `developers`
+--
 
--- Table for wishlist
-CREATE TABLE wishlist (
-    wishlist_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    product_id INT NOT NULL,
-    date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
-);
+CREATE TABLE `developers` (
+  `developer_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE INDEX idx_wishlist_user_id ON wishlist(user_id);
-CREATE INDEX idx_wishlist_product_id ON wishlist(product_id);
+--
+-- Dumping data for table `developers`
+--
 
--- Table for previous orders
-CREATE TABLE previous_orders (
-    previous_order_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    order_id INT NOT NULL,
-    completion_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    total_price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE
-);
+INSERT INTO `developers` (`developer_id`, `name`) VALUES
+(9, 'Asmodee Digital'),
+(17, 'Asobo Studio'),
+(5, 'Campo Santo'),
+(16, 'Colossal Order'),
+(6, 'ConcernedApe'),
+(8, 'Days of Wonder'),
+(20, 'EA Vancouver'),
+(7, 'Exozet'),
+(19, 'Frontier Developments'),
+(14, 'Hempuli'),
+(15, 'Maxis'),
+(10, 'Monstars Inc., Resonair'),
+(4, 'Naughty Dog'),
+(1, 'Nintendo'),
+(23, 'Nintendo EAD'),
+(18, 'Paradox Development Studio'),
+(22, 'Psyonix'),
+(3, 'Santa Monica Studio'),
+(2, 'Sony Interactive Entertainment'),
+(12, 'Thekla, Inc.'),
+(13, 'Ustwo'),
+(11, 'Valve Corporation'),
+(21, 'Visual Concepts');
 
-CREATE INDEX idx_previous_orders_user_id ON previous_orders(user_id);
-CREATE INDEX idx_previous_orders_order_id ON previous_orders(order_id);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `genre_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`genre_id`, `name`) VALUES
+(1, 'Adventure'),
+(2, 'Board'),
+(3, 'Puzzle'),
+(4, 'Simulation'),
+(5, 'Sports');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `total_price` decimal(10,2) NOT NULL,
+  `Status` enum('completed','pending','cancelled') NOT NULL DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `order_detail_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price_per_unit` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `previous_orders`
+--
+
+CREATE TABLE `previous_orders` (
+  `previous_order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `completion_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `total_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `genre_id` int(11) NOT NULL,
+  `console_type_id` int(11) NOT NULL,
+  `release_date` date NOT NULL,
+  `publisher_id` int(11) NOT NULL,
+  `developer_id` int(11) NOT NULL,
+  `stock_quantity` int(11) NOT NULL,
+  `image_path` varchar(500) NOT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publishers`
+--
+
+CREATE TABLE `publishers` (
+  `publisher_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `publishers`
+--
+
+INSERT INTO `publishers` (`publisher_id`, `name`) VALUES
+(19, '2K Sports'),
+(4, 'ConcernedApe'),
+(6, 'Days of Wonder'),
+(15, 'Electronic Arts'),
+(10, 'Enhance Games'),
+(18, 'Frontier Developments'),
+(8, 'Hans im Glück'),
+(14, 'Hempuli'),
+(1, 'Nintendo'),
+(3, 'Panic'),
+(16, 'Paradox Interactive'),
+(20, 'Psyonix'),
+(2, 'Sony Interactive Entertainment'),
+(7, 'Space Cowboys'),
+(12, 'Thekla, Inc.'),
+(5, 'United Soft Media'),
+(13, 'Ustwo'),
+(11, 'Valve Corporation'),
+(17, 'Xbox Game Studios'),
+(9, 'Z-Man Games');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `review_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `date_posted` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(512) NOT NULL COMMENT 'Store hashed passwords',
+  `date_joined` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `user_role` enum('admin','customer') NOT NULL DEFAULT 'customer',
+  `active` tinyint(4) NOT NULL DEFAULT 1 COMMENT 'Indicates if the account is active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `date_joined`, `last_updated`, `user_role`, `active`) VALUES
+(1, 'evan_leach', 'evanteam28@aston.com', 'hashed_password', '2023-11-28 23:01:46', '2023-11-28 23:01:46', 'customer', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+
+CREATE TABLE `wishlist` (
+  `wishlist_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `console_types`
+--
+ALTER TABLE `console_types`
+  ADD PRIMARY KEY (`console_type_id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `developers`
+--
+ALTER TABLE `developers`
+  ADD PRIMARY KEY (`developer_id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`genre_id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `idx_orders_user_id` (`user_id`) USING BTREE;
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`order_detail_id`),
+  ADD KEY `idx_order_details_order_id` (`order_id`),
+  ADD KEY `idx_order_details_product_id` (`product_id`) USING BTREE;
+
+--
+-- Indexes for table `previous_orders`
+--
+ALTER TABLE `previous_orders`
+  ADD PRIMARY KEY (`previous_order_id`),
+  ADD KEY `idx_previous_orders_user_id` (`user_id`) USING BTREE,
+  ADD KEY `idx_previous_orders_order_id` (`order_id`) USING BTREE;
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `fk_products_publisher` (`publisher_id`),
+  ADD KEY `fk_products_developer` (`developer_id`),
+  ADD KEY `fk_products_console_type` (`console_type_id`),
+  ADD KEY `fk_products_genre_id` (`genre_id`);
+
+--
+-- Indexes for table `publishers`
+--
+ALTER TABLE `publishers`
+  ADD PRIMARY KEY (`publisher_id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`review_id`),
+  ADD KEY `idx_reviews_product_id` (`product_id`) USING BTREE,
+  ADD KEY `idx_reviews_user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD PRIMARY KEY (`wishlist_id`),
+  ADD KEY `idx_wishlist_user_id` (`user_id`) USING BTREE,
+  ADD KEY `idx_wishlist_product_id` (`product_id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `console_types`
+--
+ALTER TABLE `console_types`
+  MODIFY `console_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `developers`
+--
+ALTER TABLE `developers`
+  MODIFY `developer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `order_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `previous_orders`
+--
+ALTER TABLE `previous_orders`
+  MODIFY `previous_order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT for table `publishers`
+--
+ALTER TABLE `publishers`
+  MODIFY `publisher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  MODIFY `wishlist_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_orders_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `fk_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `previous_orders`
+--
+ALTER TABLE `previous_orders`
+  ADD CONSTRAINT `fk_previous_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_previous_order_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_products_console_type` FOREIGN KEY (`console_type_id`) REFERENCES `console_types` (`console_type_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_products_developer` FOREIGN KEY (`developer_id`) REFERENCES `developers` (`developer_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_products_genre_id` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`genre_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_products_publisher` FOREIGN KEY (`publisher_id`) REFERENCES `publishers` (`publisher_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `fk_reviews_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_reviews_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
